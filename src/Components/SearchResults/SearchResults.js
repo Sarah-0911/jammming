f7a1d6e7563db2c99e560a2ce837cbd9c5ae98fa
Sarah-0911 <sarah.tabet69@gmail.com>
2024-02-01 13:01:01 +0100

    Convert Class component to Function component - SearchResults.js

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -3,19 +3,17 @@ import styles from './SearchResults.module.css';
 
 import TrackList from '../TrackList/TrackList';
 
-class SearchResults extends React.Component {
-  render() {
-    return (
-      <div className={styles.SearchResults}>
-        <h2>Results</h2>
-        <TrackList
-          tracks={this.props.searchResults}
-          onAdd={this.props.onAdd}
-          isRemoval={false}
-        />
-      </div>
-    )
-  }
+const SearchResults = props => {
+  return (
+    <div className={styles.SearchResults}>
+      <h2>Results</h2>
+      <TrackList
+        tracks={props.searchResults}
+        onAdd={props.onAdd}
+        isRemoval={false}
+      />
+    </div>
+  )
 }
 
 export default SearchResults;
