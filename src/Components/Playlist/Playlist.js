61227b646c3574f83b99412211e23270f919a05a
Sarah-0911 <sarah.tabet69@gmail.com>
2024-02-01 12:12:54 +0100

    Convert Class component to Function component - Playlist.js

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -2,32 +2,26 @@ import React from 'react';
 import styles from './Playlist.module.css';
 import TrackList from '../TrackList/TrackList';
 
-class Playlist extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleNameChange = this.handleNameChange.bind(this);
-  }
+const Playlist = props => {
 
-  handleNameChange(event) {
+  const handleNameChange = event => {
     const newName = event.target.value;
-    this.props.onNameChange(newName);
+    props.onNameChange(newName);
   }
 
-  render() {
-    return (
-      <div className={styles.Playlist}>
-        <input defaultValue="New Playlist" onChange={this.handleNameChange}/>
-        <TrackList
-          tracks={this.props.playlistTracks}
-          onRemove={this.props.onRemove}
-          isRemoval={true}
-        />
-        <button className={styles.PlaylistSave} onClick={this.props.onSave}>
-          SAVE TO SPOTIFY
-        </button>
-      </div>
-    )
-  }
+  return (
+    <div className={styles.Playlist}>
+      <input defaultValue="New Playlist" onChange={handleNameChange}/>
+      <TrackList
+        tracks={props.playlistTracks}
+        onRemove={props.onRemove}
+        isRemoval={true}
+      />
+      <button className={styles.PlaylistSave} onClick={props.onSave}>
+        SAVE TO SPOTIFY
+      </button>
+    </div>
+  )
 }
 
 export default Playlist;
